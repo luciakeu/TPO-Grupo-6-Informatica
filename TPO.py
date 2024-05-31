454b8b1e9a1952a574699a67ca4b6063f815acd9
luciakeu <marialuciakeu@gmail.com>
2024-05-31 11:31:20 -0300

    Update TPO.py

--- TPO.py
+++ TPO.py
@@ -28,3 +28,12 @@ def ingresar_ventas():
         
     return ventas
 
+def metododeinsercion(lista):
+    for i in range (1, len(lista)):
+        aux = lista[i]
+        j=i
+        while j>0 and lista[j-1]> aux:
+            lista [j] = lista[j-1]
+            j = j-1
+        lista[j] = aux
+            
